f1b5a11757702953f288f4ac563fff6b298109ae
Alanamiael <alanamiael.sid@gmail.com>
2023-12-25 13:23:12 +0200

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+        project: './tsconfig.json',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'react'],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'react/function-component-definition': 0,
+    // "import/no-absolute-path": 0,
+    // "import/prefer-default-export": 0,
+    // "react-hooks/exhaustive-deps": 0,
+    // "no-param-reassign": 0,
+  },
+};
---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: '@assets',
+        replacement: path.resolve(__dirname, './src/assets'),
+      },
+      {
+        find: '@components',
+        replacement: path.resolve(__dirname, './src/components'),
+      },
+      {
+        find: '@ts',
+        replacement: path.resolve(__dirname, './src/ts'),
+      },
+      {
+        find: '@styles',
+        replacement: path.resolve(__dirname, './src/styles'),
+      },
+    ],
+  },
+});
